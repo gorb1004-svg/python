1ff53446beb10ad2a98c65aba577654d613f0002
박해규 <gorb1004@naver.com>
2025-09-11 11:36:57 +0900

    break 문 추가

--- 10_break_pass.py
+++ 10_break_pass.py
@@ -1,12 +1,20 @@
-cup = 0
-running = True
+# cup = 0
+# running = True
+#
+# while True:
+#     cup += 1 ##실행순서 1번
+#     print(cup) ##실행순서 2번
+#     if cup == 10: ##실행순서 3번
+#         #running = false
+#         break ##실행순서 4번
 
-while True:
-    cup += 1
-    print(cup)
-    if cup == 10:
-        #running = false
-        break
 
-print('while 문 종료')
+for i in range(1,10):
+    if i == 3:
+        continue
+    print(i)
 
+for i in range(1,10):
+    if (i == 3:) or (i ==)
+        continue
+    print(i)
