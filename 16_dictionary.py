9d55f68c78d79e7fbdcb4e17a41d39e9c53e6140
박해규 <gorb1004@naver.com>
2025-09-12 11:15:09 +0900

    dic

--- 16_dictionary.py
+++ 16_dictionary.py
@@ -23,7 +23,7 @@ print(dic2)
 
 
 # 사전에 데이터 넣기 1
-a = {'first':'a'}
+a = {'first':'a'}          # 딕셔널리{키:밸류}
 print(a)
 
 # 사전에 데이터 넣기 2
@@ -34,4 +34,10 @@ print(a)
 del a['second']
 print(a)
 
+# 사전의 특정 요소를 꺼내보자(사용법은 List 와 비슷하다)
+print(dic2['name'])
+print(dic2['friends'])
+print(dic2.get('phone'))
 
+# 특정 키가 없는 경우 NOne 이 아닌 대체 내용으로 변환할 수 있음.
+print(dic2.get('nick','해당 내용이 없음'))
