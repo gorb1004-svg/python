a6b835a09cbe0b6570f1f85a43ccaa31cefec7a7
박해규 <gorb1004@naver.com>
2025-09-15 17:56:02 +0900

    클래스 맴버

---
+++ advance/oper.py
@@ -0,0 +1,24 @@
+#모듈은 opp
+# -> 객체지향형 프로그램
+# -> 내소스를 다른 사람들에게 가져다 쓰게 하겠다.<모듈의 개념>
+# -> 모듈을 만들고 다른 파일을 가져다 쓰게 하겠어요.
+
+# 변수
+field1 = 10                      # 가상의 변수를 작성 함.
+field2 = 'string'
+
+
+#함수
+def sum(a,b):                    # def 정의하다. sum썸을
+    return a+b                   # 반환한다 a+b
+
+def minus(a,b):
+    return a-b
+
+def multiply(a,b):
+    return a*b
+
+def devision(a,b):
+    return a/b
+
+
