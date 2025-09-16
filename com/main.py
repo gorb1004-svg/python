b6180a4327a9bc446a0fc297a880946b516ff6c7
박해규 <gorb1004@naver.com>
2025-09-16 11:51:48 +0900

    패키기 호출

---
+++ com/main.py
@@ -0,0 +1,11 @@
+from com.py.oop.other_module import other_function
+other_function()
+# ->
+
+from com.py.oop.import other_module as mo
+mo. other_function()
+
+# -> 반찬통 다 꺼내서 너가 먹을것만 뜯어서 먹어라.
+##아 얘가 이모듈에 이함수를 썼구나.
+
+
